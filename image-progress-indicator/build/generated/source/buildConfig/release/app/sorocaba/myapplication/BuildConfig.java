7d3b1eee3a7eceb169ac2969559e79867f590f03
Andrey Neto <andrey.neto@ibm.com>
2020-07-27 22:25:53 -0300

    undo packages move

---
+++ image-progress-indicator/build/generated/source/buildConfig/release/app/sorocaba/myapplication/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.sorocaba.myapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "app.sorocaba.myapplication";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
